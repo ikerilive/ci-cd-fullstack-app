eaa7fcf9b1ecd35ce5d02d47bffeb5bc3609a5f0
AF2228 <AF2228@student.jamk.fi>
2025-07-14 16:44:22 +0300

    Added test automation to workflow

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-
+const express = require('express');
 const app = express();
-app.use(cors());
-app.get("/", (req, res) => res.send("API is running"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+}
+
+module.exports = app;
